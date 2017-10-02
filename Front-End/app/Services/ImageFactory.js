ef13163ae77dac2770174ffe656b713250fde487
zceaton <zceaton@buffalo.edu>
2017-10-02 15:29:57 -0400

    Adding a dummy endpoint to work with

--- Front-End/app/Services/ImageFactory.js
+++ Front-End/app/Services/ImageFactory.js
@@ -10,33 +10,21 @@ function ImageFactory($http){
     var file;    
     return {
             encodeImage: function(file){
-                // var reader = new FileReader();
-                // reader.onload = function(loadEvent){
-                //     file = loadEvent.target.result;
-                //     //uploadFile();
-                //     //console.log(file);
-                // };
-                // reader.readAsDataURL(file);
-               return $http.get("http://localhost:8000/ccopencv/colonycount");
+                 var reader = new FileReader();
+                 reader.onload = function(loadEvent){
+                    file = loadEvent.target.result;
+                    uploadFile();
+                 };
+               var encodedFile = reader.readAsDataURL(file);
+               if (file === null || file === "" || !file){
+                alert("No file has been uploaded");
+                return;
+                }
+               return $http.post("http://localhost:8000/ccopencv/colonycount", {'file' : encodedFile,
+                                                                                'type' : file.type});
             },
                 
             uploadFile: function(){
-                    if (file === null || file === "" || !file){
-                        alert("No file has been uploaded");
-                        return;
-                    }
-                    
-                    $http({
-                        method: 'GET',
-                        url: '^hello/'
-                      }).then(function successCallback(response) {
-                          // this callback will be called asynchronously
-                          // when the response is available
-                        }, function errorCallback(response) {
-                          // called asynchronously if an error occurs
-                          // or server returns response with an error status.
-                        });
-
                     var dtx = eval("(" + atob($scope.file.substring("data:application/json;base64,".length)) + ")");
                 
                     $http.get('yourScript.php?data=' + encodeURIComponent(JSON.stringify(dtx))).then(function(response){
