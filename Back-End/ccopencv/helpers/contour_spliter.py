6d512a2b69300721a6fb5966fbad5c564e427386
Matthew Grant <mggrant@buffalo.edu>
2017-10-04 22:36:39 -0400

    contour_spliter: watershedlabels

--- Back-End/ccopencv/helpers/contour_spliter.py
+++ Back-End/ccopencv/helpers/contour_spliter.py
@@ -1,18 +1,110 @@
+import cv2
 
 
 class ContourSpliter(object):
 
     def split(self, contour_family, categ):
-        pass
+    # unsigned int hi = contour_fams.size();
+    # std::vector<std::vector<ContourFamily> > list_contour_fams(hi);
 
-    def makeWatershedLabel(self):
-        pass
+    # #pragma omp parallel for schedule(dynamic)
+    # for(unsigned int i=0; i < hi; ++i ){
+    #     signed char cat_it = categ[i];
+    #     std::vector<ContourFamily> tmp_v = list_contour_fams[i];
+    #     if(cat_it == 'M'){
+    #         splitOneCont(contour_fams[i], tmp_v);
+    #     }
+    #     else if(cat_it == 'S' ){
+    #         tmp_v.resize(1);
+    #         tmp_v[0] = contour_fams[i];
+    #     }
+    #     list_contour_fams[i] = tmp_v;
+    # }
+#     unsigned int siz = 0;
+#     for(unsigned int i=0; i < hi; ++i )
+#         siz += list_contour_fams[i].size();
 
-    def findPeaks(self):
-        pass
+#     std::vector<ContourFamily> tmp_contour_fams(siz);
+#     std::vector<signed char> tmp_categ(siz);
 
-    def splitOneCont(self):
-        pass
+#     unsigned int k = 0;
+
+
+#     for(unsigned int i=0; i < hi; ++i ){
+#         unsigned int ncl = list_contour_fams[i].size();
+
+#         for(unsigned int j=0; j < ncl; ++j ){
+#             tmp_contour_fams[k] = list_contour_fams[i][j];
+#             tmp_categ[k] = categ[i];
+#             tmp_contour_fams[k].n_per_clust = ncl;
+#             k++;
+
+#         }
+#     }
+
+#     std::swap(tmp_contour_fams,contour_fams);
+#     std::swap(tmp_categ,categ);
+# }
+
+        for idx,k in enumerate(contour_family):
+            cat_it = categ[idx]
+            tmp_v = list_contour_fams[idx];
+            if cat_it == 'M':
+                tmp_v = self.splitOneCont(k)
+            elif cat_it == 'S':
+                tmp_v[0] = contour_fams[idx]
+
+
+    def makeWatershedLabel(self, binary, peaks_conts): # set = labels
+        """
+
+        """
+        _, labels = cv2.threshold(binary, 0, 1, cv2.THRESH_BINARY)
+        for idx, k in peaks_conts:
+            cv2.drawContours(labels, peaks_conts, idx, (idx+2,0,0), thickness = -1, lineType = 8)
+        return labels
+
+    def findPeaks(self, binary, distance_map, peaks_conts):
+#     void ContourSpliter::findPeaks(const cv::Mat& binary, cv::Mat& distance_map, cont_chunk& peaks_conts){
+#         cv::Mat tmp_mat,peaks;
+#         cv::distanceTransform(binary,distance_map,CV_DIST_L2,CV_DIST_MASK_5);
+#         cv::dilate(distance_map,peaks,cv::Mat(),cv::Point(-1,-1),3);
+#         cv::dilate(binary,tmp_mat,cv::Mat(),cv::Point(-1,-1),3);
+#         peaks = peaks - distance_map;
+#         cv::threshold(peaks,peaks,0,255,cv::THRESH_BINARY);
+#         peaks.convertTo(peaks,CV_8U);
+#         cv::bitwise_xor(peaks,tmp_mat,peaks);
+#         cv::dilate(peaks,peaks,cv::Mat(),cv::Point(-1,-1),1);
+#         cv::findContours(peaks, peaks_conts, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+#         distance_map.convertTo(distance_map,CV_8U);
+# }
+
+    def splitOneCont(self, contour_fam):
+    #     cv::Rect rect = cv::boundingRect(in.contours[0]);
+    #     cv::Mat miniTmp(rect.height,rect.width,CV_8U,cv::Scalar(0));
+    #     cv::drawContours(miniTmp,in.contours,-1,cv::Scalar(255),-1,8,in.hierarchies, INT_MAX, cv::Point(-rect.x,-rect.y));
+    #     cv::copyMakeBorder(miniTmp,miniTmp, 4,4,4,4, cv::BORDER_CONSTANT, cv::Scalar(0));
+
+    #     cv::Mat distance_map;
+    #     cont_chunk peaks;
+    #     findPeaks(miniTmp,distance_map,peaks);
+
+    #     unsigned int n_peaks = peaks.size();
+    #     cv::Mat label_mat;
+    #     makeWatershedLabel(miniTmp,peaks,label_mat);
+    #     watershedLike(label_mat,distance_map,n_peaks,1.6);
+    #     std::vector<ContourFamily> tmp_out;
+    #     tmp_out.reserve(n_peaks);
+    #     for(unsigned int k=0;k != n_peaks; ++k){
+    #         cv::Mat tmp;
+    #         cont_chunk tmpc;
+    #         cv::inRange(label_mat, k+2,k+2, tmp);
+    #         cv::findContours(tmp, tmpc, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE,cv::Point(rect.x-4,rect.y-4));
+    #         tmp_out.push_back(ContourFamily(tmpc));
+    #     }
+    #     std::swap(out,tmp_out);
+    # }
+
 
     def watershedLike(self):
         pass
--- Back-End/ccopencv/step4.py
+++ Back-End/ccopencv/step4.py
@@ -30,46 +30,40 @@ class step4(step3):
         """
         print('starting pass two ...')
         print('self.cont_groups size: ', len(self.cont_groups))
-        self.makeContourChunksArray(self.input_img)
+
+        self.makeContourChunksVect(self.input_img)
         self.cont_groups = self.preFilterContourSize(self.cont_groups)
         feature_matrix = self.makeFeaturesMatrix(self.cont_groups)
-
         categ = self.predictor.predict(feature_matrix)
 
         # need to implement contour spliter helper class
         # <----  TODO  ----->
         self.cont_groups , categ = contour_spliter.split(self.cont_groups, categ)
 
+        # split the contours into 2 groups (split and unsplit)
         contour_fams_split, contour_fams_unsplit = self.separateUnsplited(self.cont_groups)
 
-        # cv::Mat feature_mat_split, feature_mat_unsplit;
-        # std::vector<signed char> categ_split, categ_unsplit;
+        # predict categories for split contours
+        contour_fams_split = self.preFilterContourSize(contour_fams_split)
+        feature_mat_split = self.makeFeaturesMatrix(contour_fams_split)
+        categ_split = self.predictor.predict(feature_mat_split)
 
+        # predict categories for unsplit contours
         contour_fams_unsplit = self.preFilterContourSize(contour_fams_unsplit)
-        # self.preFilterContourSize(contour_fams_unsplit);
-
         feature_mat_unsplit = self.makeFeaturesMatrix(contour_fams_unsplit)
-        # this->makeFeaturesMatrix(contour_fams_unsplit, feature_mat_unsplit);
-
         categ_unsplit = self.predictor_ps.predict(feature_mat_unsplit)
-        # m_predictor_ps.predict(feature_mat_unsplit, categ_unsplit);
 
-        # add contour_fams_unsplit into contour_fams_split
+        # combine contour_fams_unsplit into contour_fams_split
         # contour_fams_split.insert( contour_fams_split.end(), contour_fams_unsplit.begin(), contour_fams_unsplit.end() );
-        [contour_fams_split.append(k) for k in contour_fams_unsplit]
-
-
         # std::swap(contour_fams_split, contour_fams);
-        self.cont_groups = contour_fams_split
+        self.cont_groups = contour_fams_split + contour_fams_unsplit
 
+        # combine categ_split and unsplit
         # categ_split.insert( categ_split.end(), categ_unsplit.begin(), categ_unsplit.end() );
-        categ_split = []
-        [categ_split.append(k) for k in categ_unsplit]
-
         # std::swap(categ_split, categ);
-        categ = categ_split
+        categ = categ_split + categ_unsplit
 
-        # writeNumResults(contour_fams, categ);
+        self.writeNumResults(self.cont_groups, categ);
         # m_step_result = (void*) &m_step_numerical_result
 
     def preFilterContourSize(self, cont_groups):
@@ -95,7 +89,7 @@ class step4(step3):
         else:
             _, thrd = cv2.threshold(src, options.threshold, 255, cv2.THRES_BINARY)
 
-        _, contours, hierachies, cv2.findContours(thrd, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE):
+        _, contours, hierachies = cv2.findContours(thrd, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE):
 
         # look at step3 code
         contours = np.array(contours)
@@ -119,14 +113,26 @@ class step4(step3):
             write results for step,
         """
         print('writeNumResults ...')
+            std::vector<unsigned int> valid_idx;
+        # for(unsigned int i=0;i < categ.size() ; i++)
+        #     if(categ[i] == 'S')
+        #         valid_idx.push_back(i);
+
+        # DEV_INFOS(valid_idx.size());
+        # m_step_numerical_result.reset(valid_idx.size());
+
+        # for(unsigned int i=0;i < valid_idx.size() ; i++){
+        #      unsigned int idx = valid_idx[i];
+        #         m_step_numerical_result.add_at(OneObjectRow(contour_fams[idx],m_raw_img),i);
+        #     }
         valid_idx = []
-        for i in categ:
-            if i = 'S':
+        for i,c  in enumerate(categ):
+            if c = 'S':
                 valid_idx.append(i)
         print('valid_idx size: ', len(valid_idx))
 
-        for i in valid_idx:
-            pass
+        for i, in enumerate(valid_idx):
+            idx = valid_idx[i]
             # OneObjectRow  draws contours on raw image
             # m_step_numerical_result.add_at(OneObjectRow(contour_fams[idx], m_raw_img),i);
 
