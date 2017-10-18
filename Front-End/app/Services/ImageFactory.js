d03039b6832daf680f2ea6de44f839fddb6dbc42
abarganier <abargani@gmail.com>
2017-10-18 15:52:21 -0400

    Comments for ImageFactory

--- Front-End/app/Services/ImageFactory.js
+++ Front-End/app/Services/ImageFactory.js
@@ -1,3 +1,8 @@
+/**
+ * This service acts as a singleton object (google it) that can be shared across many controllers.
+ * It's main purpose is to pull files into memory and convert them to base64 to be sent to the server
+ * for processing. 
+ */
 angular
     .module('App.ImageFactory', [])
     .factory('ImageFactory', ImageFactory);
@@ -9,8 +14,21 @@ ImageFactory.$inject = [
 function ImageFactory($http){
     var postUrl = 'http://localhost:8000/colonycount/';
     return {
+        /**
+         * Array to store results in for display
+         */
         results: [],
+
+        /**
+         * Takes in a file object containing metadata, and a callback to execute
+         * once the file has been uploaded. We pass the POST request object and 
+         * the file's base64 to the callback for further handling.
+         */ 
         encodeImage: function(file, onLoadCallback){
+            /** Read about the FileReader here: 
+             * https://stackoverflow.com/questions/36280818/how-to-convert-file-to-base64-in-javascript
+             * https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+             */
             var reader = new FileReader();
             var that = this;
             reader.onload = function(loadEvent) {
@@ -24,6 +42,9 @@ function ImageFactory($http){
             }
             reader.readAsDataURL(file);
         },
+        /**
+         * Given a filename, returns the file's extension (i.e. 'png')
+         */
         getFileExtension: function(fileName) {
             var split = fileName.split('.');
             return split[split.length-1];
