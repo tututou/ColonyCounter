07595b05c1ef4c56a6dce79dd1d56374bab12562
Alex Barganier <abarganier@users.noreply.github.com>
2017-10-20 15:10:12 -0400

    Style home page to look more like a landing page (#67)
    * Main layout of landing page set
    
    * Finish landing page layout
    
    * Additional coloring

--- Front-End/app/app.js
+++ Front-End/app/app.js
@@ -61,10 +61,47 @@ angular.module('App', [
                 }
             }
         });
-    
+        
     $urlRouterProvider.otherwise('/');
+    $mdThemingProvider.definePalette('mcgpalette1', {
+  '50': 'e4f2fe',
+  '100': 'bce0fb',
+  '200': '90cbf9',
+  '300': '64b6f7',
+  '400': '42a6f5',
+  '500': '2196f3',
+  '600': '1d8ef1',
+  '700': '1883ef',
+  '800': '1479ed',
+  '900': '0b68ea',
+  'A100': 'ffffff',
+  'A200': 'e1ecff',
+  'A400': 'aeccff',
+  'A700': '95bcff',
+  'contrastDefaultColor': 'light',
+  'contrastDarkColors': [
+    '50',
+    '100',
+    '200',
+    '300',
+    '400',
+    'A100',
+    'A200',
+    'A400',
+    'A700'
+  ],
+  'contrastLightColors': [
+    '500',
+    '600',
+    '700',
+    '800',
+    '900'
+  ]
+});
 
-    $mdThemingProvider.theme('default');
+    
+  $mdThemingProvider.theme('default')
+    .primaryPalette('mcgpalette1')
 })
 .run(function(){
 
